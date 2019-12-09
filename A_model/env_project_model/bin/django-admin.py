0ae5e3f47ba21000c0bb889e12fc629f3e002482
Fadhil Abdulkarim <fadhil@alterra.id>
2019-12-09 10:49:27 +0700

    added files for django project w4

---
+++ A_model/env_project_model/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/alta14/Desktop/AltA/django_trial/DJANGO_MVC/A_model/env_project_model/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ C_advance_views/env_project_advance_views/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/alta14/Desktop/AltA/django_trial/DJANGO_MVC/C_advance_views/env_project_advance_views/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()

a68619baf23b34ea2e7c672c3b566bd04df921a8
Fadhil Abdulkarim <fadhil@alterra.id>
2019-12-09 16:40:14 +0700

    added magic __str__ to preview rows

--- A_model/project_model/hospital/models.py
+++ A_model/project_model/hospital/models.py
@@ -8,17 +8,25 @@ class Dokter(models.Model):
     nomor_telepon = models.CharField(max_length=11)
     bidang = models.CharField(max_length=200)
     jadwal_praktek = models.CharField(max_length=200)
+    def __str__(self):
+        return self.nama
 
 class Pasien(models.Model):
     nama = models.CharField(max_length=200)
     nomor_telepon = models.CharField(max_length=11)
     alamat = models.CharField(max_length=200)
     keluhan = models.CharField(max_length=200)
+    def __str__(self):
+        return self.nama
 class Resep(models.Model):
     nama = models.CharField(max_length=200)
     total_harga = models.DecimalField(max_digits=25, decimal_places=2)
     kumpulan_obat = models.CharField(max_length=200)
+    def __str__(self):
+        return self.nama
 class Obat(models.Model):
     nama = models.CharField(max_length=200)
     kandungan = models.CharField(max_length=200)
     khasiat = models.CharField(max_length=200)
+    def __str__(self):
+        return self.nama
--- A_model/project_model/zoo/models.py
+++ A_model/project_model/zoo/models.py
@@ -8,15 +8,23 @@ class Hewan(models.Model):
     species = models.CharField(max_length=200)
     berat = models.DecimalField(max_digits=8, decimal_places=2)
     umur = models.CharField(max_length=200)
+    def __str__(self):
+        return self.nama
 class Kandang(models.Model):
     nama = models.CharField(max_length=200)
     isi_kandang = models.CharField(max_length=200)
     luas_kandang = models.DecimalField(max_digits=25, decimal_places=2)
+    def __str__(self):
+        return self.nama
 class Penjaga(models.Model):
     nama = models.CharField(max_length=200)
     nomor_telepon = models.CharField(max_length=11)
     jadwal_jaga = models.CharField(max_length=200)
+    def __str__(self):
+        return self.nama
 class Pengunjung(models.Model):
     nama = models.CharField(max_length=200)
     nomor_telepon = models.CharField(max_length=11)
     hari_berkunjung = models.DateField('Hari Berkunjung')
+    def __str__(self):
+        return self.nama

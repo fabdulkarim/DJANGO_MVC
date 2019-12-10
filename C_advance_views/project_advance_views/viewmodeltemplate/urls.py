da317f8a28792ed492843627bc7ffe110253c687
Fadhil Abdulkarim <fadhil@alterra.id>
2019-12-10 20:09:48 +0700

    added early addition to total structure, html not yet working and other stuffs

---
+++ C_advance_views/project_advance_views/viewmodeltemplate/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+app_name = 'viewmodeltemplate'
+urlpatterns = [
+    path('blog', views.blog , name='blog'),
+    path('mentor', views.mentor , name='mentor'),
+    path('mentee', views.mentee , name='mentee'),
+    path('blog/<int:blog_id>', views.blogpost , name='blogpost'),
+    path('blog/input', views.blog_input , name='blog_input')
+]
---
+++ C_advance_views/project_advance_views/viewmodeltemplate/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+from .models import Blog, Mentor, Mentee 
+# Create your views here.
+
+def blog(request):
+    blog_object = Blog.objects.all()
+    return render(request, 'viewmodeltemplate/blog.html', {
+        'blog_object': blog_object
+    })
+
+def mentor(request):
+    mentor_obj = Mentor.objects.all()
+    return render(request, 'viewmodeltemplate/mentor.html', {
+        'mentor_obj': mentor_obj
+    })
+
+def mentee(request):
+    mentee_obj = Mentee.objects.all()
+    return render(request, 'viewmodeltemplate/mentee.html', {
+        'mentee_obj': mentee_obj
+    })
+
+def blogpost(request, blog_id):
+    blogpost_obj = Blog.objects.get(pk=blog_id)
+    return render(request, 'viewmodeltemplate/blogpost.html',{
+        'blogpost_obj': blogpost_obj
+    })
+
+def blog_input(request):
+    return render(request, 'viewmodeltemplate/blog_input.html',{})
+
+
